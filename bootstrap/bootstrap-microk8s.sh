8b0debf344206f284a41b82d827741102f7e01b4
Dan Foster <danfoster@locussoftware.com>
2024-03-30 23:48:43 +0000

    initial commit

---
+++ bootstrap/bootstrap-microk8s.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+usage() {
+    echo "$0 HOSTNAME"
+    echo
+    echo "$0 is used for bootstrapping microk8s clusters. SSH access is required"
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+NODENAME=$1
+TMP=$(mktemp -d)
+
+echo $TMP
+ssh $NODENAME sudo microk8s config > $TMP/remote.config
